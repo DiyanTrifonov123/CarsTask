65399a7df6c7eaff039d0e994feadfe0615edd56
DiyanTrifonov123 <diyantrifonov5@gmail.com>
2023-05-10 11:35:04 +0300

    First commit

---
+++ CarsZadacha/Car.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsZadacha
+{
+    internal class Car
+    {
+        private string brand;
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private string licensePlate;
+
+        public string LicensePlate
+        {
+            get { return licensePlate; }
+            set { licensePlate = value; }
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+            
+        }
+
+        private static int counter;
+
+        public static int Counter
+        {
+            get { return counter; }
+        }
+
+        public Car(string brand, string model, string license, int year)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.LicensePlate = license;
+            this.Year = year;
+            counter++;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.Brand} {this.Model}: brum-brum-brum!");
+        }
+
+        public void IsEco()
+        {
+            if (this.Year >= 2000)
+            {
+                Console.WriteLine($"{this.Brand} {this.Model} е екологична!");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Brand} {this.Model} не е екологична!");
+            }
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Аз съм {this.Brand} {this.Model} с регистрационен номер" +
+                $" {this.LicensePlate} и съм роден {this.Year} година!");
+        }
+    }
+}
---
+++ CarsZadacha/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsZadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Искаш ли да вкараш кола?: ");
+            string command = Console.ReadLine().ToLower();
+            List<Car> carsList = new List<Car>();
+            int i = 1;
+
+            while (command != "no" && command != "ne" && command != "не")
+            {
+                Console.Write($"Въведи марка на {i} кола: ");
+                string brand = Console.ReadLine();
+
+                Console.Write($"Въведи модел на {i} кола: ");
+                string model = Console.ReadLine();
+
+                Console.Write($"Въведи регистрационен номер на {i} кола: ");
+                string license = Console.ReadLine();
+
+                Console.Write($"Въведи година на {i} кола: ");
+                int year = int.Parse(Console.ReadLine());
+
+                Car car = new Car(brand, model, license, year);
+                carsList.Add(car);
+                Console.WriteLine();
+
+                Console.Write("Искаш ли да продължиш да вкарваш коли?: ");
+                command = Console.ReadLine().ToLower();
+                Console.WriteLine();
+                i++;
+            }
+
+            foreach (var item in carsList)
+            {
+                item.Print();
+                item.IsEco();
+                item.Introduce();
+                Console.WriteLine();
+            }
+
+            if (Car.Counter > 1)
+            {
+                Console.WriteLine($"{Car.Counter} коли се състезават!");
+                Console.WriteLine();
+            }            
+        }
+    }
+}
